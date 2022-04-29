b2bc353c410d78947d2c1bed1e86b6a7b0091a40
yourf <yourfogottengod@gmail.com>
2022-04-29 23:53:08 +1000

    add clean architecture packages

--- app/src/main/java/com/example/cleanwater_project/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cleanwater_project/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cleanwater_project
+package com.example.cleanwater_project.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cleanwater_project.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
