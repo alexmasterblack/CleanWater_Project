4b9e3ce2cc163026520362156e40e18540ac6306
yourf <yourfogottengod@gmail.com>
2022-05-21 15:06:18 +1000

    add division into fragments

---
+++ app/src/main/java/com/example/cleanwater_project/presentation/fragments/MainInfoFragment.kt
@@ -0,0 +1,6 @@
+package com.example.cleanwater_project.presentation.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.cleanwater_project.R
+
+class MainInfoFragment : Fragment(R.layout.main_info_fragment) {}
